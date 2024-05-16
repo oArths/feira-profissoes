3548afa2230d93f20e65c227bc168e2bd7453778
unknown <arthurferreira1417@gmail.com>
2024-05-15 22:06:28 -0300

    Feat: create a input componet

---
+++ app/(tabs)/style.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components/native";
+
+export const Body = styled.View`
+display: flex;
+align-items: center;
+width: 100%;
+height: 100%;
+gap: 60px;
+`;
+export const Header = styled.View`
+margin-top: 30%;
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 20px;
+`;
+export const Title = styled.Text`
+font-size: 40px;
+color: wheat;
+`;
+export const SubTitle = styled.Text`
+font-size: 40px;
+color: wheat;
+`;
+export const Main = styled.View`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+width: 80%;
+gap: 10px;
+`;
