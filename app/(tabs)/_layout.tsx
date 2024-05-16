67e0a113e44b15dc99b9d584fd73aa9cfeb32692
unknown <arthurferreira1417@gmail.com>
2024-05-15 22:05:36 -0300

    Feat: update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,9 +26,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Registros',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'book-outline' : 'book-outline'} color={color} />
           ),
         }}
       />
