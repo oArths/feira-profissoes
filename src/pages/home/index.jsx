36cab5c9851743d4602312397afcb648ebb46b2f
Emerson757 <emerson0757@gmail.com>
2024-05-17 17:13:22 -0300

    feat: add a home page card

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,16 @@
 import * as S from "./style"
 import Header from "../../componets/header";
 import Button from "../../componets/button";
+import Card  from "../../componets/card/index";
 import { useNavigation } from "@react-navigation/native";
 const Home = ()=> {
     const nav = useNavigation();
     return(
         <S.Body>
                     <Header op={true} SubTitle="Perfis Cadastrados"/>
+                    <S.CardContainer>
+                    <Card/>
+                    </S.CardContainer>
                         <Button  onPress={() => {nav.navigate("Register")}} Title="Novo Cadastro"/> 
 
         </S.Body>
--- src/pages/home/style.jsx
+++ src/pages/home/style.jsx
@@ -2,5 +2,15 @@ import styled from "styled-components/native";
 
 export const Body = styled.View`
 width: 100%;
+height: 100%;
 height: max-content;
+gap: 20px;
+`;
+export const CardContainer = styled.View`
+display: flex;
+flex-direction: column;
+gap: 20px;
+width: 90%;
+height: max-content;
+
 `;
