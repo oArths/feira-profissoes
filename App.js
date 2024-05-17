2602a8552a9e18972d949641d34480b912674939
unknown <arthurferreira1417@gmail.com>
2024-05-17 16:08:24 -0300

    Feat: add a routes in app

--- App.js
+++ App.js
@@ -1,5 +1,33 @@
-import CreativeComponent from "./src/class-component/native-component";
+import { StatusBar } from "expo-status-bar";
+import Home from "./src/pages/home";
+import Register from "./src/pages/register";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { GlobalStyle } from './global.jsx';
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./theme";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <CreativeComponent />;
+  return (
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+      {/* <StatusBar style="default" backgroundColor="black"  /> */}
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: null }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{ title: null }}
+          />
+          
+        </Stack.Navigator>
+      </NavigationContainer>
+      </ThemeProvider>
+  );
 }
