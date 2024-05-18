bb037204f01e3781b4c440ce84645e39552dec7f
unknown <arthurferreira1417@gmail.com>
2024-05-17 22:05:25 -0300

    New: finalize style in register page

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,25 +1,44 @@
-import * as S from "./style"
+import * as S from "./style";
 import Header from "../../componets/header";
 import Button from "../../componets/button";
 import Input from "../../componets/input";
 import { useNavigation } from "@react-navigation/native";
+import { Platform} from "react-native";
 
-const Register = ()=> {
-    const nav = useNavigation();
+const Register = () => {
+  const nav = useNavigation();
 
-    return(
-<S.Body>
-                    <Header SubTitle="Cadastro de Perfil" onPress={() => {nav.navigate("Home")}}/>
-                        <Input onChange={""} Title="Nome:"/>
-                        <Input onChange={""} Title="Email:"/>
-                        <Input onChange={""} Title="Data de Nascimento:"/>
-                        <Input onChange={""} Title="Profissão dos Sonhos:"/>
-                        <Input onChange={""} Title="Seu Bairro:"/>
-                        <S.ButtonContainer>
-                        <Button  onPress={() => {nav.navigate("Home")}} Title="Cadastrar"/> 
-                        </S.ButtonContainer>
-
-        </S.Body>
-    )
-}
+  return (
+    <S.Body
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={10}
+    >
+      <S.Scroll>
+       <S.Main>
+       <S.HeaderConatiner>
+       <Header
+          SubTitle="Cadastro de Perfil"
+          onPress={() => {
+            nav.navigate("Home");
+          }}
+        />
+       </S.HeaderConatiner>
+        <Input onChange={""} Title="Nome:" />
+        <Input onChange={""} Title="Email:" />
+        <Input onChange={""} Title="Data de Nascimento:" />
+        <Input onChange={""} Title="Profissão dos Sonhos:" />
+        <Input onChange={""} Title="Seu Bairro:" />
+        <S.ButtonContainer>
+          <Button
+            onPress={() => {
+              nav.navigate("Home");
+            }}
+            Title="Cadastrar"
+          />
+        </S.ButtonContainer>
+       </S.Main>
+      </S.Scroll>
+    </S.Body>
+  );
+};
 export default Register;
--- src/pages/register/style.jsx
+++ src/pages/register/style.jsx
@@ -1,15 +1,25 @@
 import styled from "styled-components/native";
 
-export const Body = styled.View`
-display: flex;
-flex-direction: column;
-/* justify-content: center; */
-align-items: center;
-width: 100%;
-height: max-content;
+export const Body = styled.KeyboardAvoidingView`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: max-content;
 `;
-
+export const Scroll = styled.ScrollView`
+width: 100%;
+`;
+export const Main = styled.View`
+  display: flex;
+  align-items: center;
+`;
+export const HeaderConatiner = styled.View`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100%;
+`;
 export const ButtonContainer = styled.View`
-width: 80%;
-
+  width: 80%;
 `;
