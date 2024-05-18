a3f775c53cfb7d91e497100056ef97458a9c4c3b
unknown <arthurferreira1417@gmail.com>
2024-05-17 22:04:56 -0300

    New: add a cards and button new register

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,20 +1,63 @@
-import * as S from "./style"
+import * as S from "./style";
+import React, { useState } from 'react';
 import Header from "../../componets/header";
 import Button from "../../componets/button";
-import Card  from "../../componets/card/index";
+import Card from "../../componets/card/index";
 import { useNavigation } from "@react-navigation/native";
-const Home = ()=> {
-    const nav = useNavigation();
-    return(
-        <S.Body>
-                    <Header op={true} SubTitle="Perfis Cadastrados"/>
-                    <S.CardContainer>
-                    <Card/>
-                    </S.CardContainer>
-                        <Button  onPress={() => {nav.navigate("Register")}} Title="Novo Cadastro"/> 
+import ModalOp from "../../componets/modal";
+const Home = () => {
+  const nav = useNavigation();
+  const [modalDeleteVisible, setModalDeleteVisible] = useState(false);
 
-        </S.Body>
+  const abrirModalDelete = () => {
+    setModalDeleteVisible(true);
+  };
 
-    )
-}
+  const fecharModalDelete = () => {
+    setModalDeleteVisible(false);
+  };
+ 
+    const personData = {
+      name: 'John Dogggggggggggggggglkke',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+    };
+  return (
+    <S.Body>
+    <S.Main>
+        <S.Conatiner>
+            <S.HeaderConatiner>
+            <Header op={true} SubTitle="Perfis Cadastrados" />
+            </S.HeaderConatiner>
+      
+      <S.CardContainer>
+        <Card 
+        data={personData.name} 
+        nome={personData.name} 
+        bairro={personData.name} 
+        sonho={personData.name}
+        onPressDelete={abrirModalDelete}
+        />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </S.CardContainer>
+      
+      </S.Conatiner>
+    </S.Main>
+    <S.ButtonContainer>
+      <Button
+        onPress={() => {
+          nav.navigate("Register");
+        }}
+        Title="Novo Cadastro"
+      />
+      </S.ButtonContainer>
+      <ModalOp   isVisible={modalDeleteVisible} onClose={fecharModalDelete}/>
+
+    </S.Body>
+  );
+};
 export default Home;
--- src/pages/home/style.jsx
+++ src/pages/home/style.jsx
@@ -1,16 +1,37 @@
 import styled from "styled-components/native";
-
 export const Body = styled.View`
+display: flex;
+align-items: center;
+`;
+export const Main = styled.ScrollView`
 width: 100%;
 height: 100%;
-height: max-content;
 gap: 20px;
+`;
+export const HeaderConatiner = styled.View`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 80%;
+`;
+export const Conatiner = styled.View`
+  display: flex;
+  align-items: center;
+  gap: 30px;
 `;
 export const CardContainer = styled.View`
 display: flex;
 flex-direction: column;
+align-items: center;
 gap: 20px;
-width: 90%;
+width: 100%;
+padding-bottom: 100px;
 height: max-content;
 
+`;
+export const ButtonContainer = styled.View`
+position: absolute;
+z-index: 1;
+bottom: 50px;
+  width: 60%;
 `;
