0d58cb424ef9d3456ff93f693fdd3a1609b6f0d8
unknown <arthurferreira1417@gmail.com>
2024-05-21 22:25:21 -0300

    Feat: add

--- src/componets/date/style.jsx
+++ src/componets/date/style.jsx
@@ -1,26 +1,35 @@
 import styled from "styled-components/native";
 import { theme } from "../../../theme";
 
-export const Conatiner= styled.View`
+export const Conatiner = styled.View`
 display: flex;
 flex-direction: column;
 justify-content: center;
-align-items: center;
+align-items: start;
 width: 100%;
 gap: 5px;
 margin-bottom: 30px;
 
 `;
-export const ContainerInput = styled.View`
+export const ConatinerInput = styled.View`
 display: flex;
-flex-direction: column;
-width: 80%;
+flex-direction: row;
+width: 100%;
+
+`;
+export const IconConatiner = styled.TouchableOpacity`
+/* position: relative; */
+width: 40px;
+height: 30px;
+/* left: 10px;
+top: 5px; */
 `;
 export const Title =  styled.Text`
 font-size: 16px;
 `;
-export const Input = styled.TextInput`
+export const Input = styled.View`
     width: 100%;
+    height: 40px;
   padding: 5px;
   color: black;
   border: 2px solid ${(props) => theme.colors.cinzaa};
@@ -29,4 +38,3 @@ export const Input = styled.TextInput`
 
   
 `;
-
