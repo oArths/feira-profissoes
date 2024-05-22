7e2cafb02c3ed10d85a18a1cff156efbdbac1f49
unknown <arthurferreira1417@gmail.com>
2024-05-21 22:25:27 -0300

    Feat: add

--- src/componets/modal/index.jsx
+++ src/componets/modal/index.jsx
@@ -14,26 +14,19 @@ const ModalOp = ({ isVisible, onClose }) => {
         <S.ModalOut onPressIn={onClose}>
           <S.ModalIn>
             <S.Container>
-                <S.Text>Deseja Excluir?</S.Text>
-            <S.CloseModal onPress={onClose}>
-            <I.Xmark
-            color="#000"
-            width={'30px'}
-            height={'30px'}
+            <I.XmarkCircle
+            color="#E10613"
+            width={'50px'}
+            height={'50px'}
             />
-            </S.CloseModal>
+                <S.Text>Oops...</S.Text>
             </S.Container>
-            <S.TextOP>Após a exlusão os dados{'\n'}serão perdidos permanentemente </S.TextOP>
+            <S.TextOP>Preencha todos os campos!</S.TextOP>
             <S.ContainerButton>
-              <S.ConfirmOP
-                onPress={onClose}
-              >
-                <S.ButtonText>Cancelar</S.ButtonText>
-              </S.ConfirmOP>
               <S.Confirm
                 onPress={onClose}
                 >
-                <S.ButtonText>Excluir</S.ButtonText>
+                <S.ButtonText>OK</S.ButtonText>
               </S.Confirm>
             </S.ContainerButton>
           </S.ModalIn>
