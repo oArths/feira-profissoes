b28a70c835974e42d93dd6f700ac45787a7439b0
unknown <arthurferreira1417@gmail.com>
2024-05-22 22:18:05 -0300

    Feat: update

--- src/componets/date/style.jsx
+++ src/componets/date/style.jsx
@@ -2,39 +2,40 @@ import styled from "styled-components/native";
 import { theme } from "../../../theme";
 
 export const Conatiner = styled.View`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: start;
-width: 100%;
-gap: 5px;
-margin-bottom: 30px;
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  width: 100%;
+  gap: 5px;
+  margin-bottom: 30px;
+`;
+export const Title = styled.Text`
+  font-size: 16px;
 `;
 export const ConatinerInput = styled.View`
-display: flex;
-flex-direction: row;
-width: 100%;
-
+  display: flex;
+  flex-direction: row;
+  width: 100%;
 `;
 export const IconConatiner = styled.TouchableOpacity`
-/* position: relative; */
-width: 40px;
-height: 30px;
-/* left: 10px;
-top: 5px; */
-`;
-export const Title =  styled.Text`
-font-size: 16px;
+  width: 40px;
+  height: 30px;
 `;
 export const Input = styled.View`
-    width: 100%;
-    height: 40px;
+  display: flex;
+  flex-direction: row;
+
+  width: 100%;
+  height: 40px;
   padding: 5px;
   color: black;
   border: 2px solid ${(props) => theme.colors.cinzaa};
   border-radius: 10px;
-  max-width: 100%;
-
-  
+`;
+export const Data = styled.Text`
+  margin-top: 5px;
+  width: 200px;
+  height: 20px;
+  font-size: 15px;
 `;
