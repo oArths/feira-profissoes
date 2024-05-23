04ecb0183550062438924d8876ea58eb3de6fe4b
unknown <arthurferreira1417@gmail.com>
2024-05-22 22:17:35 -0300

    Feat: create a new data

--- src/componets/date/date.jsx
+++ src/componets/date/date.jsx
@@ -1,27 +1,19 @@
-import * as S from "./style"
+import * as S from "./style";
 import * as I from "iconoir-react-native";
 
-
-const DatePiker = ({onPress, Title, Data}) => {
-    return(
-        <S.Conatiner>
-            <S.Title>{Title}</S.Title>
-            <S.ConatinerInput>
-            
-            <S.Input>
-            <S.IconConatiner onPress={onPress}>
-            <I.Calendar
-            color="#E10613"
-            width={'30px'}
-            height={'30px'}
-            
-            />
-            </S.IconConatiner>
-                {/* {Data} */}
-            </S.Input>
-            </S.ConatinerInput>
-
-        </S.Conatiner>
-    )
-} 
+const DatePiker = ({ onPress, Title, Data }) => {
+  return (
+    <S.Conatiner>
+      <S.Title>{Title}</S.Title>
+      <S.ConatinerInput>
+        <S.Input>
+          <S.IconConatiner onPress={onPress}>
+            <I.Calendar color="#E10613" width={"30px"} height={"30px"} />
+          </S.IconConatiner>
+          <S.Data>{Data}</S.Data>
+        </S.Input>
+      </S.ConatinerInput>
+    </S.Conatiner>
+  );
+};
 export default DatePiker;
