9e1e357824b451b98a8715ad5b42af58d74ad923
Arthur <arthurferreira1417@gmail.com>
2024-05-23 16:29:21 -0300

    Feat:limpando o compoente

--- src/componets/header/index.jsx
+++ src/componets/header/index.jsx
@@ -1,48 +1,48 @@
 import * as S from "./style"
 import { NavArrowLeft } from 'iconoir-react-native';
-    const Header = ({SubTitle, op, onPress})=> {
-    return(
+const Header = ({ SubTitle, op, onPress }) => {
+    return (
         <S.Header>
             {op == true ? (<S.Conatainer >
                 <S.ConatinerImg>
-                    <S.Logo   source={require("../../img/Logo.png")}/>
+                    <S.Logo source={require("../../img/Logo.png")} />
                 </S.ConatinerImg>
                 <S.ConatinerImg>
-                    <S.Slug   source={require("../../img/slug.png")}/>
+                    <S.Slug source={require("../../img/slug.png")} />
                 </S.ConatinerImg>
-            </S.Conatainer>): 
-            ((<S.Op>
-                <S.ConatainerOp >
-                <S.ConatinerImgOp>
-                    <S.LogoOp   source={require("../../img/Logo.png")}/>
-                </S.ConatinerImgOp>
-                <S.ConatinerImgOp>
-                    <S.Slug   source={require("../../img/SlugOp.png")}/>
-                </S.ConatinerImgOp>
-            </S.ConatainerOp>
-            <S.ConatinerBack onPress={onPress}>
-                <NavArrowLeft color="black" height={36} width={36} />
-            </S.ConatinerBack>
-            </S.Op>
-        
-        ))}
+            </S.Conatainer>) :
+                ((<S.Op>
+                    <S.ConatainerOp >
+                        <S.ConatinerImgOp>
+                            <S.LogoOp source={require("../../img/Logo.png")} />
+                        </S.ConatinerImgOp>
+                        <S.ConatinerImgOp>
+                            <S.Slug source={require("../../img/SlugOp.png")} />
+                        </S.ConatinerImgOp>
+                    </S.ConatainerOp>
+                    <S.ConatinerBack onPress={onPress}>
+                        <NavArrowLeft color="black" height={36} width={36} />
+                    </S.ConatinerBack>
+                </S.Op>
 
-        
-        {op == true ? (<S.ConatainerText>
-            <S.ConatinerTitle>
-            <S.Title>Demonstração{'\n'}de Funcionalidade</S.Title>
-            <S.Line/>
-            </S.ConatinerTitle>
-            <S.SubTitle>{SubTitle}</S.SubTitle>
-        </S.ConatainerText>) :(
-             <S.ConatainerTextOP>
-             <S.ConatinerTitleOP>
-             <S.Title>Demonstração{'\n'}de Funalidade</S.Title>
-             <S.Line/>
-             </S.ConatinerTitleOP>
-             <S.SubTitle>{SubTitle}</S.SubTitle>
-         </S.ConatainerTextOP>
-        ) }
+                ))}
+
+
+            {op == true ? (<S.ConatainerText>
+                <S.ConatinerTitle>
+                    <S.Title>Demonstração{'\n'}de Funcionalidade</S.Title>
+                    <S.Line />
+                </S.ConatinerTitle>
+                <S.SubTitle>{SubTitle}</S.SubTitle>
+            </S.ConatainerText>) : (
+                <S.ConatainerTextOP>
+                    <S.ConatinerTitleOP>
+                        <S.Title>Demonstração{'\n'}de Funcionalidade</S.Title>
+                        <S.Line />
+                    </S.ConatinerTitleOP>
+                    <S.SubTitle>{SubTitle}</S.SubTitle>
+                </S.ConatainerTextOP>
+            )}
         </S.Header>
     )
 }
